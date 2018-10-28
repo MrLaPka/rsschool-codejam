0ed20cad69c40968d3b1008456113d792d57f2e1
Egor <42974925+MrLaPka@users.noreply.github.com>
2018-10-28 16:53:31 +0300

    Delete sumOfOther.js

--- sumOfOther/sumOfOther.js
+++
@@ -1,8 +0,0 @@
-module.exports = function sumOfOther(arr){
- let result = arr.reduce(function(sum,item){
-return sum+item;
- }, 0);
- return  arr.map(function(item){
-     return result-item;
- })
-}
